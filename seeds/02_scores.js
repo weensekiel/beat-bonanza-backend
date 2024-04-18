e8afcc42851999471ac5bebff7fc37c8fdc03b5b
Ezekiel <evangelista.ezekiel@hotmail.ca>
2024-04-17 23:16:53 -0400

    edit seed file

--- seeds/02_scores.js
+++ seeds/02_scores.js
@@ -5,21 +5,4 @@
 export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('scores').del()
-  await knex('scores').insert([
-    {
-      id: 1, 
-      user_id: 1,
-      score: 511020
-    },
-    {
-      id: 2, 
-      user_id: 1,
-      score: 494580
-    },
-    {
-      id: 3, 
-      user_id: 1,
-      score: 456800
-    }
-  ]);
 };
